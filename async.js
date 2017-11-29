dff0492649672c559b44c690c5327ad13b6cf9f2
iriskis <irinka199701@mail.ru>
2017-11-30 02:10:54 +0500

    impleneted

--- async.js
+++ async.js
@@ -7,7 +7,44 @@ exports.runParallel = runParallel;
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    return new Promise(resolve => {
+        if (!jobs.length) {
+            resolve([]);
+
+            return;
+        }
+        const results = [];
+        let countRun = 0;
+        let countFinished = 0;
+
+        while (countRun < parallelNum) {
+            run(countRun++);
+        }
+
+        function run(jobsNumber) {
+            new Promise((resolveJob, rejectJob) => {
+                jobs[jobsNumber]().then(resolveJob, rejectJob);
+                setTimeout(rejectJob, timeout, new Error('Promise timeout'));
+            })
+                .then(result => writeResult(result, jobsNumber),
+                    result => writeResult(result, jobsNumber)
+                );
+        }
+
+        function writeResult(result, jobsNumber) {
+            results[jobsNumber] = result;
+            countFinished++;
+            if (countFinished === jobs.length) {
+                resolve(results);
+
+                return;
+            }
+            if (countRun < jobs.length) {
+                run(countRun++);
+            }
+        }
+    });
 }
